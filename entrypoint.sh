b652e690c951e55410fef08f2dd05b524df1591f
Sergey Ladanov <sergei_ladanov_93@mail.ru>
2024-01-16 11:32:14 +0300

    add

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,6 +2,6 @@
 
  . /app/app_py_env/bin/activate
 
- RESULT_VERSION="$(exec python3 /app/$USING_FILE @)"
+ RESULT_VERSION="$(exec python3 /app/$USING_FILE $@)"
 
  echo "RESULT_VERSION=$RESULT_VERSION" >> $GITHUB_OUTPUT
